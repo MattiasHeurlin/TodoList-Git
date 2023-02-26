ca13840bf287ee3c53cfb4c3fb0423f6876d1915
Mattias heurlin <mattias.heurlin@hotmail.com>
2023-02-26 17:40:35 +0100

    Script Setup Base

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <script lang="ts">
-  import { defineComponent, ref } from 'vue'
+  import { defineComponent, reactive } from 'vue'
 
   export default defineComponent({
-    name: 'App',
+    name: 'TodoApp',
     setup() {
-     
+
+    interface Todo {
+      text: string,
+      done: boolean
+    }
+
+    const state = reactive({
+              todos: [
+                { text: 'Learn Vue', completed: false },
+                { text: 'Build an app', completed: false },
+                { text: 'Deploy app to production', completed: false }
+              ],
+               newTodoText: '',
+              });
+              function addNewTodo() {
+                state.todos.push({
+                  text: state.newTodoText,
+                  completed: false
+                });
+                state.newTodoText = '';
+              }
+              function removeTodo(index: number) {
+                state.todos.splice(index, 1);
+              }
+              function toggleTodoStatus(index: number) {
+                state.todos[index].completed = !state.todos[index].completed;
+              }
+
+
       return {
-       
+        state,
+        addNewTodo,
+        removeTodo,
+        toggleTodoStatus
+
       }
     }
   })
