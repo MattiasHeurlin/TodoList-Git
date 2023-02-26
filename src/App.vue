208f34d8f523205b8ba6946b419888dcf1f25044
Mattias heurlin <mattias.heurlin@hotmail.com>
2023-02-26 17:27:21 +0100

    First base commit - setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { defineComponent, ref } from 'vue'
+
+  export default defineComponent({
+    name: 'App',
+    setup() {
+     
+      return {
+       
+      }
+    }
+  })
+  
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
